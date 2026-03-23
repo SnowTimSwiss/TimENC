e3edbdf6badc921b2d34a7601e227749f181e71c
Tim <tim.bleuer301@gmail.com>
2026-03-23 12:00:18 +0100

    fixes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -94,6 +94,9 @@ async fn create_keyfile(output_path: PathBuf) -> Result<OperationResult, String>
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
             encrypt_file,
             decrypt_file,
